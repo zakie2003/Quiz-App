d1ccbc0d2065f2c81f743a008175f862148d9b27
zakie2003 <zk.khan2003@gmail.com>
2025-02-15 18:07:59 +0530

    Testing 2

--- Mad2_Flask/app.py
+++ Mad2_Flask/app.py
@@ -98,7 +98,7 @@ def save_otp():
     except Exception as e:
         return jsonify({"status":500,"message":f"{e}"})
 
-app.route("/hi",methods=["GET"])
+@app.route("/hi",methods=["GET"])
 def hi():
     return jsonify({"status":200,"message":"Hello"})
 
